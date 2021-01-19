1692bd3accdb8eb1f7e295b83045d1610c0c0d61
Mike Lin <dna@mlin.net>
2021-01-18 23:08:15 -1000

    guess a ref range for each segment based on mappings

--- src/view.rs
+++ src/view.rs
@@ -1,7 +1,9 @@
 use clap::Clap;
+use io::Write;
 use json::JsonValue;
 use log::{debug, info, warn};
-use rusqlite::{params, OpenFlags, NO_PARAMS};
+use num_format::{Locale, ToFormattedString};
+use rusqlite::{params, OpenFlags, OptionalExtension, NO_PARAMS};
 use std::{env, fs, io, path, process};
 
 use crate::bad_command;
@@ -21,6 +23,9 @@ pub struct Opts {
     /// Launch Bandage on output file (temporary file, if unspecified)
     #[clap(long)]
     pub bandage: bool,
+    /// For each segment with reference mappings, set gr:Z tag with one guessed range summarizing the mappings
+    #[clap(long)]
+    pub guess_ranges: bool,
 }
 
 pub fn main(opts: &Opts) -> Result<()> {
@@ -31,7 +36,7 @@ pub fn main(opts: &Opts) -> Result<()> {
     dbopts.insert("immutable", json::JsonValue::from(true));
 
     // open db
-    let db = genomicsqlite::open(
+    let mut db = genomicsqlite::open(
         &opts.input_gfab,
         OpenFlags::SQLITE_OPEN_READ_ONLY | OpenFlags::SQLITE_OPEN_NO_MUTEX,
         &dbopts,
@@ -40,31 +45,53 @@ pub fn main(opts: &Opts) -> Result<()> {
     debug!("gfabase v{} created {}", gfab_version, opts.input_gfab);
     util::check_gfab_version(&gfab_version)?;
 
-    if opts.output_gfa == "-" && !opts.bandage && atty::is(atty::Stream::Stdout) {
-        // interactive mode: pipe into less -S
-        less(|less_in| {
-            write_header(&db, less_in)
-                .and_then(|_| write_segments(&db, "", !opts.no_sequences, less_in))
-                .and_then(|_| write_links(&db, "", less_in))
-                .and_then(|_| write_paths(&db, "", less_in))
-        })?
-    } else {
-        let mut output_gfa = String::from(&opts.output_gfa);
-        if opts.bandage && output_gfa == "-" {
-            output_gfa = bandage_temp_filename()?
-        }
+    {
+        let txn = db.transaction()?;
+        let mut maybe_guesser = if opts.guess_ranges {
+            Some(SegmentRangeGuesser::new(&txn, "")?)
+        } else {
+            None
+        };
+        let mut tag_editor = |segment_id: i64, tags: &mut json::JsonValue| -> Result<()> {
+            if let Some(ref mut guesser) = maybe_guesser {
+                if let Some(gr) = guesser.get(segment_id)? {
+                    tags.insert("gr:Z", gr).unwrap()
+                }
+            }
+            Ok(())
+        };
 
-        {
-            let mut writer_box = writer(&output_gfa)?;
-            let out = &mut *writer_box;
-            write_header(&db, out)?;
-            write_segments(&db, "", !opts.no_sequences, out)?;
-            write_links(&db, "", out)?;
-            write_paths(&db, "", out)?
-        }
+        if opts.output_gfa == "-" && !opts.bandage && atty::is(atty::Stream::Stdout) {
+            // interactive mode: pipe into less -S
+            less(|less_in| {
+                write_header(&txn, less_in)
+                    .and_then(|_| {
+                        write_segments(&txn, "", !opts.no_sequences, &mut tag_editor, less_in)
+                    })
+                    .and_then(|_| write_links(&txn, "", less_in))
+                    .and_then(|_| write_paths(&txn, "", less_in))
+            })?
+        } else {
+            let mut output_gfa = String::from(&opts.output_gfa);
+            if opts.bandage && output_gfa == "-" {
+                output_gfa = bandage_temp_filename()?
+            }
 
-        if opts.bandage {
-            bandage(&output_gfa)?
+            {
+                let mut writer_box = writer(&output_gfa)?;
+                let out = &mut *writer_box;
+                write_header(&txn, out)?;
+                write_segments(&txn, "", !opts.no_sequences, &mut tag_editor, out)?;
+                write_links(&txn, "", out)?;
+                write_paths(&txn, "", out)?
+            }
+
+            if opts.bandage {
+                if let Some(ref mut guesser) = maybe_guesser {
+                    guesser.write_bandage_csv(&output_gfa)?
+                }
+                bandage(&output_gfa)?
+            }
         }
     }
 
@@ -151,6 +178,7 @@ pub fn write_segments(
     db: &rusqlite::Connection,
     where_clause: &str,
     with_sequences: bool,
+    mut tag_editor: impl FnMut(i64, &mut json::JsonValue) -> Result<()>,
     writer: &mut dyn io::Write,
 ) -> Result<()> {
     let segments_query_sql = String::from(if with_sequences {
@@ -180,7 +208,13 @@ pub fn write_segments(
         if let Some(sequence_length) = maybe_sequence_length {
             writer.write_fmt(format_args!("\tLN:i:{}", sequence_length))?;
         }
-        write_tags("gfa1_segments_meta", rowid, &tags_json, writer)?;
+        write_tags_with_editor(
+            "gfa1_segments_meta",
+            rowid,
+            &tags_json,
+            &mut tag_editor,
+            writer,
+        )?;
         writer.write(b"\n")?;
     }
     Ok(())
@@ -286,13 +320,20 @@ pub fn write_paths(
     Ok(())
 }
 
-fn write_tags(table: &str, rowid: i64, tags_json: &str, writer: &mut dyn io::Write) -> Result<()> {
+fn write_tags_with_editor(
+    table: &str,
+    rowid: i64,
+    tags_json: &str,
+    mut editor: impl FnMut(i64, &mut json::JsonValue) -> Result<()>,
+    writer: &mut dyn io::Write,
+) -> Result<()> {
     let invalid = || util::Error::InvalidGfab {
         message: String::from("invalid tags_json"),
         table: String::from(table),
         rowid: rowid,
     };
-    let tags = json::parse(&tags_json).map_err(|_| invalid())?;
+    let mut tags = json::parse(&tags_json).map_err(|_| invalid())?;
+    editor(rowid, &mut tags)?;
     for (k, v) in tags.entries() {
         let kfields: Vec<&str> = k.split(':').collect();
         if kfields.len() != 2 {
@@ -309,4 +350,111 @@ fn write_tags(table: &str, rowid: i64, tags_json: &str, writer: &mut dyn io::Wri
         writer.write_fmt(format_args!("\t{}:{}", k, vstr))?;
     }
     Ok(())
+}
+
+fn write_tags(table: &str, rowid: i64, tags_json: &str, writer: &mut dyn io::Write) -> Result<()> {
+    write_tags_with_editor(table, rowid, tags_json, |_, _| Ok(()), writer)
+}
+
+// Helpers roughly guessing a genomic range for a segment based on its PAF mappings. Selects the
+// chromosome with the most coverage in the mappings, then the min and max mapped position on that
+// chromosome.
+pub struct SegmentRangeGuesser<'a> {
+    getter: rusqlite::Statement<'a>,
+    csv_query: rusqlite::Statement<'a>,
+}
+
+impl<'a> SegmentRangeGuesser<'_> {
+    pub fn new(
+        db: &'a rusqlite::Connection,
+        where_clause: &str,
+    ) -> Result<SegmentRangeGuesser<'a>> {
+        // analyze mappings to generate temp.segment_range_guess
+        db.execute(
+            "CREATE TABLE temp.segment_range_guess(
+                segment_id INTEGER PRIMARY KEY,
+                refseq_name TEXT NOT NULL,
+                refseq_begin INTEGER NOT NULL, refseq_end INTEGER NOT NULL)",
+            NO_PARAMS,
+        )?;
+        let sql = format!(
+            "WITH summary AS
+                (SELECT
+                    segment_id, refseq_name,
+                    min(refseq_begin) AS min_begin, max(refseq_end) AS max_end,
+                    sum(refseq_end - refseq_begin) AS coverage
+                FROM gfa1_segment_mapping
+                {}
+                GROUP BY segment_id, refseq_name)
+             INSERT INTO temp.segment_range_guess(segment_id, refseq_name, refseq_begin, refseq_end)
+                SELECT segment_id, refseq_name, min_begin, max_end
+                FROM
+                    (SELECT
+                        segment_id, refseq_name, min_begin, max_end,
+                        row_number() OVER (PARTITION BY segment_id ORDER BY coverage DESC)
+                            AS coverage_rank
+                    FROM summary)
+                WHERE coverage_rank = 1",
+            where_clause
+        );
+        let n = db.execute(&sql, NO_PARAMS)?;
+        info!("guessed ranges for {} segments", n);
+        // prepare queries on temp.segment_range_guess
+        Ok(SegmentRangeGuesser {
+            getter: db.prepare(
+                "SELECT refseq_name, refseq_begin, refseq_end
+                 FROM temp.segment_range_guess WHERE segment_id = ?",
+            )?,
+            csv_query: db.prepare(
+                "SELECT
+                    coalesce(name, cast(segment_id AS TEXT)),
+                    refseq_name, refseq_begin, refseq_end
+                 FROM temp.segment_range_guess LEFT JOIN gfa1_segment_meta USING(segment_id)",
+            )?,
+        })
+    }
+
+    pub fn get(&mut self, segment_id: i64) -> Result<Option<String>> {
+        let maybe_row: Option<(String, i64, i64)> = self
+            .getter
+            .query_row(params![segment_id], |row| {
+                Ok((row.get(0)?, row.get(1)?, row.get(2)?))
+            })
+            .optional()?;
+        if let Some((refseq_name, refseq_begin, refseq_end)) = maybe_row {
+            return Ok(Some(format!(
+                "{}:{}-{}",
+                refseq_name,
+                refseq_begin.to_formatted_string(&Locale::en),
+                refseq_end.to_formatted_string(&Locale::en)
+            )));
+        }
+        Ok(None)
+    }
+
+    pub fn write_bandage_csv(&mut self, gfa_filename: &str) -> Result<()> {
+        // write a CSV file with the guessed ranges that Bandage can show as labels
+        let csv_filename = String::from(gfa_filename.strip_suffix(".gfa").unwrap_or(gfa_filename))
+            + ".guessed_ranges.csv";
+        {
+            let mut writer = io::BufWriter::new(fs::File::create(&csv_filename)?);
+            writer.write_fmt(format_args!("Name,Guessed range\n"))?;
+            let mut cursor = self.csv_query.query(NO_PARAMS)?;
+            while let Some(row) = cursor.next()? {
+                let name: String = row.get(0)?;
+                let refseq_name: String = row.get(1)?;
+                let refseq_begin: i64 = row.get(2)?;
+                let refseq_end: i64 = row.get(3)?;
+                writer.write_fmt(format_args!(
+                    "\"{}\",\"~{}:{}-{}\"\n",
+                    name,
+                    refseq_name,
+                    refseq_begin.to_formatted_string(&Locale::en),
+                    refseq_end.to_formatted_string(&Locale::en)
+                ))?;
+            }
+        }
+        info!("wrote CSV with guessed segment ranges to {}", csv_filename);
+        Ok(())
+    }
 }
