9151dfa76cd2de5292dae0652a96a7c0063614dd
Mike Lin <dna@mlin.net>
2021-02-15 22:12:05 -1000

    reduce size of link indexes
    ...in exchange for graph crawling speed. The trade-off should be moderate if the links are loaded in a sane order.

--- src/schema/GFA1.index.sql
+++ src/schema/GFA1.index.sql
@@ -4,15 +4,15 @@ CREATE UNIQUE INDEX gfa1_segment_name ON
 CREATE INDEX gfa1_segment_mapping_segment ON
     gfa1_segment_mapping(segment_id);
 
-CREATE INDEX gfa1_link_from_to ON
-    gfa1_link(from_segment,to_segment);
-CREATE INDEX gfa1_link_to_from ON
-    gfa1_link(to_segment,from_segment);
+CREATE INDEX gfa1_link_from ON
+    gfa1_link(from_segment);
+CREATE INDEX gfa1_link_to ON
+    gfa1_link(to_segment);
 
-CREATE INDEX gfa1_containment_container_contained ON
-    gfa1_containment(container_segment,contained_segment);
-CREATE INDEX gfa1_containment_contained_container ON
-    gfa1_containment(contained_segment,container_segment);
+CREATE INDEX gfa1_containment_container ON
+    gfa1_containment(container_segment);
+CREATE INDEX gfa1_containment_contained ON
+    gfa1_containment(contained_segment);
 
 CREATE UNIQUE INDEX gfa1_path_name ON
     gfa1_path(name) WHERE name IS NOT NULL;
